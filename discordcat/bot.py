e2cc38e4e0ee5ec7bbae7334d9b9bc906e8ab1c8
Krystian Postek <krystian@postek.eu>
2021-09-26 15:25:10 +0200

    Create system command

--- discordcat/bot.py
+++ discordcat/bot.py
@@ -1,9 +1,10 @@
 from lightbulb import Bot
 
-from discordcat.configure import SetupCommand
-from discordcat.services import env
-from discordcat.verifing import VerifyCommand, VerifyForceCommand
-from discordcat.manage import ManageGroup
+from .configure import SetupCommand
+from .services import env
+from .verifing import VerifyCommand, VerifyForceCommand
+from .manage import ManageGroup
+from .system import SystemCheckCommand
 
 bot = Bot(token=env.get("DISCORD_TOKEN"), slash_commands_only=True, banner=None, logs="DEBUG")
 
@@ -12,3 +13,4 @@ bot.add_slash_command(SetupCommand)
 bot.add_slash_command(VerifyCommand)
 bot.add_slash_command(VerifyForceCommand)
 bot.add_slash_command(ManageGroup)
+bot.add_slash_command(SystemCheckCommand)
---
+++ discordcat/system.py
@@ -0,0 +1,27 @@
+import json
+import platform
+from datetime import datetime
+
+from lightbulb import SlashCommand, SlashCommandContext
+
+from .checks import superusers_only
+from .services import db
+
+
+class SystemCheckCommand(SlashCommand):
+    name = 'system'
+    description = 'Pokazuje stan systemu'
+
+    async def callback(self, context: SlashCommandContext):
+        response = {
+            "time": datetime.now().isoformat(),
+            "platform": platform.platform(),
+            "mongo": {
+                "name": db.name,
+                "client": str(db.client)
+            }
+        }
+
+        await context.respond(f"```json\n{json.dumps(response, indent=2)}\n```")
+
+    checks = [superusers_only]
