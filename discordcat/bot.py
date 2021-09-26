c5abb395e1960aaeb2067522071efc3d1051e0be
Krystian Postek <krystian@postek.eu>
2021-09-26 13:01:30 +0200

    Add command to manage users

--- discordcat/bot.py
+++ discordcat/bot.py
@@ -2,16 +2,13 @@ from lightbulb import Bot
 
 from discordcat.configure import SetupCommand
 from discordcat.services import env
-from discordcat.update import UpdateCommand
-from discordcat.verifing import VerifyCommand
+from discordcat.verifing import VerifyCommand, VerifyForceCommand
 from discordcat.manage import ManageGroup
 
-bot = Bot(token=env.get("DISCORD_TOKEN"), slash_commands_only=True, banner=None)
+bot = Bot(token=env.get("DISCORD_TOKEN"), slash_commands_only=True, banner=None, logs="DEBUG")
 
 
 bot.add_slash_command(SetupCommand)
-bot.add_slash_command(UpdateCommand)
 bot.add_slash_command(VerifyCommand)
+bot.add_slash_command(VerifyForceCommand)
 bot.add_slash_command(ManageGroup)
-
-bot.run()
--- discordcat/manage.py
+++ discordcat/manage.py
@@ -1,5 +1,7 @@
+from hikari import User
 from lightbulb import SlashSubCommand, SlashCommandGroup, Option, SlashCommandContext
 
+from .checks import operator_only, verified_only
 from .services import db
 
 
@@ -9,7 +11,7 @@ class ManageGroup(SlashCommandGroup):
 
 
 @ManageGroup.subcommand()
-class SetupVerifiedRoleCommand(SlashSubCommand):
+class ManageSelfCommand(SlashSubCommand):
     name = "self"
     description = "Zarządzaj swoim kontem"
 
@@ -25,3 +27,26 @@ class SetupVerifiedRoleCommand(SlashSubCommand):
             await context.respond("w dowodzie se sprawdź")
         else:
             await context.respond("ayo???")
+
+    checks = [verified_only]
+
+
+@ManageGroup.subcommand()
+class ManageSomeone(SlashSubCommand):
+    name = "user"
+    description = "Zarządzaj innym użytkownikiem"
+
+    user: User = Option('Użytkownik')
+    action: str = Option('Operacja', choices=['remove', 'info'])
+
+    async def callback(self, context: SlashCommandContext):
+        user = context.options['user'].value
+        action = context.options['action'].value
+
+        if action == 'remove':
+            db["verified"].delete_many({"discord_id": user})
+            await context.respond("Unieważniono weryfikację użytkownika.")
+        elif action == 'info':
+            await context.respond("idk")
+
+    checks = [operator_only]
