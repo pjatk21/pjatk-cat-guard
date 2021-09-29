a050406e0004d6260d785a6410c8e2f1c1f08c35
Krystian Postek <krystian@postek.eu>
2021-09-29 13:44:35 +0200

    Add initial test in module runner

--- discordcat/__main__.py
+++ discordcat/__main__.py
@@ -1,3 +1,13 @@
-from discordcat.bot import bot
+import logging
 
+from discordcat.bot import bot
+from discordcat.services import mongo
+
+local_logger = logging.getLogger('module-runner')
+
+local_logger.debug("Testing connection to the mongo...")
+mongo.list_database_names()
+local_logger.debug("Runner passed initial tests")
+
+logging.info("Starting bot")
 bot.run()
--- discordcat/services.py
+++ discordcat/services.py
@@ -9,5 +9,5 @@ load_dotenv()
 env = os.environ
 
 sg = SendGridAPIClient(env.get("SENDGRID_APIKEY"))
-mongo = MongoClient(env.get("MONGODB_URL"))
+mongo = MongoClient(env.get("MONGODB_URL"), serverSelectionTimeoutMS=10000)
 db = mongo["catguard"]
