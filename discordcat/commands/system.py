895402d3eae9f9282e32e6830208bd0309aaed70
Krystian Postek <krystian@postek.eu>
2021-10-01 13:33:40 +0200

    Add more verbose check

--- discordcat/commands/system.py
+++ discordcat/commands/system.py
@@ -13,11 +13,19 @@ class SystemCheckCommand(SlashCommand):
     description = "Pokazuje stan systemu"
 
     async def callback(self, context: SlashCommandContext):
+        from pipenv.project import Project
         response = {
             "time": datetime.now().isoformat(),
             "platform": platform.platform(),
             "mongo": {"name": db.name, "client": str(db.client)},
+            "packages": str(Project().packages),
+            "runtime": {
+                "packages": list(sorted([str(p) for p in Project().installed_packages])),
+                "version": platform.python_version(),
+                "implementation": platform.python_implementation(),
+            }
         }
+        del Project
 
         await context.respond(f"```json\n{json.dumps(response, indent=2)}\n```")
 
