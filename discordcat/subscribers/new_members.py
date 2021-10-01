e251fd544c320ba03dc7d3ffbfeec26b94c6c4ca
Krystian Postek <krystian@postek.eu>
2021-10-01 13:33:55 +0200

    Log while join

--- discordcat/subscribers/new_members.py
+++ discordcat/subscribers/new_members.py
@@ -1,3 +1,5 @@
+import logging
+
 from hikari.events import MemberCreateEvent
 from .subscriber import Subscription
 
@@ -7,6 +9,7 @@ class NewUserJoined(Subscription):
 
     @staticmethod
     async def callback(event: MemberCreateEvent):
+        logging.getLogger('new-member').debug(f'New member {event.member} joined, sending welcome message.')
         await event.member.send(
             "uwu, na tym serwerze obowiązuje weryfikacja!\n"
             'Sprawdź kanał "#✋weryfikacja-członków" aby dowiedzieć się więcej uwu'
