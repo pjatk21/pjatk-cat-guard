23fc8f78b53aefe4b8476d2738476640fa096f26
Krystian Postek <krystian@postek.eu>
2021-12-03 19:25:52 +0100

    Remove non-valid import

--- discordcat/services.py
+++ discordcat/services.py
@@ -2,12 +2,10 @@ import os
 
 from dotenv import load_dotenv
 from pymongo import MongoClient
-from sendgrid import SendGridAPIClient
 
 load_dotenv()
 
 env = os.environ
 
-sg = SendGridAPIClient(env.get("SENDGRID_APIKEY"))
 mongo = MongoClient(env.get("MONGODB_URL"), serverSelectionTimeoutMS=5000)
 db = mongo["catguard"]
