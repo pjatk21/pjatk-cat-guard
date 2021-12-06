9dd2857de0f2ea02427c221964767220105ed7ed
Krystian Postek <krystian@postek.eu>
2021-12-06 11:30:08 +0100

    Minor fix in webgate

--- webgate/invites.py
+++ webgate/invites.py
@@ -1,13 +1,12 @@
+import os
 from datetime import timedelta
 
 from hikari import RESTApp
 
-from discordcat.services import env
-
 
 async def gen_guild_invite(channel_id: int):
     rest_app = RESTApp()
-    async with rest_app.acquire(env.get("DISCORD_TOKEN"), "Bot") as bot:
+    async with rest_app.acquire(os.getenv("DISCORD_TOKEN"), "Bot") as bot:
         return await bot.create_invite(
             channel_id, max_uses=1, max_age=timedelta(minutes=5), reason="autogen"
         )
