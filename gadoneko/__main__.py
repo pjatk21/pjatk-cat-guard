747e3492df7b52abe8e020ea89ccf2ce75c84e2e
Krystian Postek <krystian@postek.eu>
2021-12-09 13:05:25 +0100

    Mark version

--- gadoneko/__main__.py
+++ gadoneko/__main__.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 import sentry_sdk as sentry
+from requests import __version__
 
 from gadoneko.bot import bot
 from shared.db import init_connection
@@ -10,6 +11,6 @@ init_connection()
 
 if os.getenv('SENTRY'):  # Init error reporting to sentry
     logging.info('Sentry DSN present, init.')
-    sentry.init(os.getenv('SENTRY'), environment=os.getenv('ENV'))
+    sentry.init(os.getenv('SENTRY'), environment=os.getenv('ENV'), release=__version__)
 
 bot.run()
