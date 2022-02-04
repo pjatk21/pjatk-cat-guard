915f86a07d0db228c08baa572f5e7cb67cdeadd0
Krystian Postek <krystian@postek.eu>
2022-02-04 20:38:11 +0100

    UX admin fixes

--- webpanel/admin.py
+++ webpanel/admin.py
@@ -2,6 +2,7 @@ import os
 import re
 
 from authlib.integrations.starlette_client import OAuth
+from bson import ObjectId
 from dotenv import load_dotenv
 from hikari import RESTApp
 from mongoengine import DoesNotExist
@@ -17,7 +18,8 @@ from starlette.requests import Request
 from starlette.responses import JSONResponse, RedirectResponse, Response
 
 from shared.db import init_connection
-from shared.documents import VerificationRequest, GuildConfiguration, VerificationState, TrustedUser, VerificationMethod
+from shared.documents import VerificationRequest, GuildConfiguration, VerificationState, TrustedUser, \
+    VerificationMethod, Reviewer
 from webpanel.common import templates
 from webpanel.middleware.auth import DiscordAuthBackend
 import webpanel.tasks
@@ -56,22 +58,23 @@ async def admin_oauth(request: Request):
     async with RESTApp().acquire(token['access_token']) as client:
         user = await client.fetch_my_user()
 
-        available_guilds = [guild.id for guild in await client.fetch_my_guilds() if GuildConfiguration.objects(guild_id=guild.id).first()]
+        try:
+            rev = Reviewer.objects(identity__user_id=user.id).get()
+        except DoesNotExist:
+            return templates.TemplateResponse('message.html', {'request': request, 'header': 'Nie jesteś wyznaczony na sprawdzającego.', 'paragraphs': ['sorka']})
 
-        if not len(available_guilds):
-            return JSONResponse({'message': 'not in guilds'})
 
         request.session['discord'] = token
         request.session['user'] = str(user)
-        request.session['guilds'] = available_guilds
+        request.session['discord']['id'] = user.id
+        request.session['reviewer'] = str(rev.id)
 
     return RedirectResponse(request.url_for('admin:admin_index'))
 
 
 @app.route('/logout')
 async def admin_logout(request: Request):
-    del request.session['discord']
-    del request.session['user']
+    request.session.clear()
     return templates.TemplateResponse('message.html', {'request': request, 'header': 'wylogowano'})
 
 
@@ -109,6 +112,7 @@ class AdminReview(HTTPEndpoint):
 
         vr.trust = trust
         vr.state = VerificationState.ACCEPTED
+        vr.reviewer = ObjectId(request.session['reviewer'])
         vr.save()
 
         tasks = BackgroundTasks()
@@ -133,11 +137,12 @@ async def admin_reject(request: Request):
     tasks.add_task(webpanel.tasks.send_rejection_dm, vr, form['reason'])
 
     vr.state = VerificationState.REJECTED
+    vr.reviewer = ObjectId(request.session['reviewer'])
     vr.save()
     return RedirectResponse(request.url_for('admin:admin_index'), status_code=302, background=tasks)
 
 
-@app.route('/photoproxy/{side}-{rid}', name='photoproxy')
+@app.route('/photoproxy/{side}-{rid}.jpg', name='photoproxy')
 class PhotoProxy(HTTPEndpoint):
     @requires('authenticated')
     async def get(self, request: Request):
