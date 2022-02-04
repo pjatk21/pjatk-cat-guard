77f79a2c4e6a22015d847c0866664a18164b7445
Krystian Postek <krystian@postek.eu>
2022-02-04 16:07:06 +0100

    Add improved debug for bot

--- gadoneko/bot/__main__.py
+++ gadoneko/bot/__main__.py
@@ -13,4 +13,9 @@ if os.getenv('SENTRY'):  # Init error reporting to sentry
     logging.info('Sentry DSN present, init.')
     sentry.init(os.getenv('SENTRY'), environment=os.getenv('ENV'), release=__version__)
 
-bot.run()
+if os.getenv('ENV') == 'dev':
+    import warnings
+    warnings.simplefilter('ignore', DeprecationWarning)
+    bot.run(asyncio_debug=True, coroutine_tracking_depth=20, propagate_interrupts=True)
+else:
+    bot.run()
