d844ec57288584629ad667be7e3c6f9a679489e3
Krystian Postek <krystian@postek.eu>
2022-02-05 14:26:02 +0100

    Add sentry support

--- webpanel/__init__.py
+++ webpanel/__init__.py
@@ -1,1 +1,12 @@
+import logging
+import os
+
+import sentry_sdk
+from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
+
 from .panel import app
+
+if os.getenv('SENTRY'):
+    sentry_sdk.init(dsn=os.getenv('SENTRY'))
+    app = SentryAsgiMiddleware(app)
+    logging.info('Enabled sentry for webpanel (%s)', app)
