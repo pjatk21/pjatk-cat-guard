ae0723b01a75d5bf168739b0f206dc11afe5cafc
Krystian Postek <krystian@postek.eu>
2022-02-05 10:16:42 +0100

    Update doomsday.py

--- doomsday.py
+++ doomsday.py
@@ -2,10 +2,16 @@ import asyncio
 import os
 
 from dotenv import load_dotenv
-from hikari import GatewayBot, ShardReadyEvent, HikariError
+from hikari import GatewayBot, ShardReadyEvent, HikariError, Embed
+from mongoengine import DoesNotExist
 
+from gadoneko.plugins.trust import start_verification_flow
+
+from shared.db import init_connection
+from shared.documents import VerificationRequest, TrustedUser
 
 load_dotenv()
+init_connection()
 
 bot = GatewayBot(os.getenv('DISCORD_TOKEN'))
 
@@ -19,10 +25,27 @@ async def doomsday(event: ShardReadyEvent):
 
     for member in await bot.rest.fetch_members(guild):
         try:
-            await asyncio.gather([
+            trust: TrustedUser = TrustedUser.objects(identity__user_id=member.id, identity__guild_id=guild.id).get()
+        except DoesNotExist:
+            continue
+
+        trust.delete()
+
+        vr: VerificationRequest = start_verification_flow(guild, member)
+        link = f"{os.getenv('VERIFICATION_URL')}verify/{vr.code}"
+
+        try:
+            await asyncio.gather(*[
                 bot.rest.remove_role_from_member(
                     guild, member, role
                 ) for role in takedowns
-            ])
+            ], member.send(
+                embed=Embed(
+                    title='Nowy link weryfikacyjny!',
+                    description=f'Oto twój nowy link: {link}, przypominamy również, że rangi typ "Warszawa Dzienne", trzeba ponownie przypisać',
+                    url=link,
+                ).set_footer('doomsday')
+            ))
+            print('reset ✅', member)
         except HikariError as he:
-            print(he)
+            print(he, member)
